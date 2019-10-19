1f5c4628faa3196c0cf89a1f1f947679b03f34a7
201851026-iiitv <201851026@iiitvadodara.ac.in>
2019-10-19 21:17:40 +0530

    added participant class

--- lib/models/participant.dart
+++ lib/models/participant.dart
@@ -1,8 +1,9 @@
 import 'package:intl/intl.dart';
-class Participant{
+
+class Participant {
   String _name;
   String _gender;
   String _dob;
-  String _t-shirt;
-  
+  String _tshirt;
+  bool _isAdmin;
 }
