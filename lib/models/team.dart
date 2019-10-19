cda5555c2287667460053fef985d05dcb8a22f07
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-10-20 00:17:17 +0530

    fixed stupid user files conflict

--- lib/models/team.dart
+++ lib/models/team.dart
@@ -2,11 +2,10 @@ import 'package:timder/models/participant.dart';
 import 'package:flutter/material.dart';
 
 class Team {
-<<<<<<< HEAD
-<<<<<<< HEAD
   List<Participant> teamMembers = new List<Participant>();
   String teamName;
   int maxTeamSize;
+  List<Participant> participants = new List<Participant>();
   Team({
     @required this.teamMembers,
     @required this.teamName,
@@ -20,18 +19,4 @@ class Team {
       return true;
     }
   }
-=======
-=======
->>>>>>> 8b4cc6387f5ebed00b46c1bc7daff0c47c14e7da
-  List<Participant> participants = new List<Participant>();
-  String teamName;
-  bool isTeamFilled;
-  Team(
-      {@required this.participants,
-      @required this.teamName,
-      @required this.isTeamFilled});
-<<<<<<< HEAD
->>>>>>> 8b4cc6387f5ebed00b46c1bc7daff0c47c14e7da
-=======
->>>>>>> 8b4cc6387f5ebed00b46c1bc7daff0c47c14e7da
 }
