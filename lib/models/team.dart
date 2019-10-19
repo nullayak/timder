2e04a05db0b0f41ee1f8ab9ae9c5393c4143f650
Pushkar Patel <thepushkarp@gmail.com>
2019-10-20 03:58:59 +0530

    :art: fix conflicts in team

--- lib/models/team.dart
+++ lib/models/team.dart
@@ -2,8 +2,6 @@ import 'package:timder/models/participant.dart';
 import 'package:flutter/material.dart';
 
 class Team {
-<<<<<<< HEAD
-<<<<<<< HEAD
   List<Participant> teamMembers = new List<Participant>();
   String teamName;
   int maxTeamSize;
@@ -20,18 +18,4 @@ class Team {
       return true;
     }
   }
-=======
-=======
->>>>>>> 8b4cc6387f5ebed00b46c1bc7daff0c47c14e7da
-  List<Participant> participants = new List<Participant>();
-  String teamName;
-  bool isTeamFilled;
-  Team(
-      {@required this.participants,
-      @required this.teamName,
-      @required this.isTeamFilled});
-<<<<<<< HEAD
->>>>>>> 8b4cc6387f5ebed00b46c1bc7daff0c47c14e7da
-=======
->>>>>>> 8b4cc6387f5ebed00b46c1bc7daff0c47c14e7da
 }
